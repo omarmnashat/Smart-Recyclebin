f10a154a355efffcc4937271dfc6b255a2d6ce5e
omarmnashat <102851561+omarmnashat@users.noreply.github.com>
2023-06-19 11:47:12 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,60 @@
+
+function checkradio()
+    {var result=0;
+
+let radio1 = document.getElementById(12);
+let radio2 = document.getElementById(22);
+let radio3 = document.getElementById(31);
+let radio4 = document.getElementById(43);
+let radio5 = document.getElementById(51);
+let radio6 = document.getElementById(63);
+let radio7 = document.getElementById(71);
+let radio8 = document.getElementById(81);
+let radio9 = document.getElementById(93);
+let radio10 = document.getElementById(103);
+let radio11 = document.getElementById(111);
+let radio12 = document.getElementById(121);
+ if(radio1.checked){
+
+    result=result+1;
+ }
+ if(radio2.checked){
+    result=result+1;
+    }
+    if(radio3.checked){
+        result=result+1;
+        }
+        if(radio4.checked){
+            result=result+1;
+            }
+            if(radio5.checked){
+                result=result+1;
+                }
+                if(radio6.checked){
+                    result=result+1;
+                    }
+                    if(radio7.checked){
+                        result=result+1;
+                        }
+                        if(radio8.checked){
+                            result=result+1;
+                            }
+                            if(radio9.checked){
+                                result=result+1;
+                                }
+                                if(radio10.checked){
+                                    result=result+1;
+                                    }
+                                    if(radio11.checked){
+                                        result=result+1;
+                                        }
+                                        if(radio12.checked){
+                                            result=result+1;
+                                        }
+                                        window.alert("your score is : "+result) ;
+                                        
+     
+
+
+}
+    
